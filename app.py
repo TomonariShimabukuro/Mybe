df5a7a3f88a7ce2aaac71118946d66e1231efb89
TomonariShimabukuro <shorion11@gmail.com>
2024-01-02 10:31:43 +0900

    パンくずリストのホームを削除

--- app.py
+++ app.py
@@ -1,53 +1,49 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
+from flask_mysqldb import MySQL
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:shimaT0915%@shori.local/mybe'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'  # Set your own secret key
 
-# Creating SQLAlchemy and Flask-Admin objects without app context
-db = SQLAlchemy(app)
-admin = Admin(app)
+# MySQLの接続情報を設定
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'shimaT0915%'
+app.config['MYSQL_DB'] = 'image_gallery'
+app.config['UPLOAD_FOLDER'] = 'static/images'
 
-class Image(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255))
-    data = db.Column(db.LargeBinary)
-    description = db.Column(db.String(255))
+mysql = MySQL(app)
 
-
-
-# Creating the admin view within the app context
-with app.app_context():
-    db.create_all()
-    admin.add_view(ModelView(Image, db.session))
-
+# 画像をアップロードするためのルート
 @app.route('/')
 def index():
-    # Displaying the list of images
-    images = Image.query.all()
-    return render_template('index.html', images=images)
+    # データベースから画像のリストを取得
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM images")
+    
+    # タプルのリストからディクショナリのリストに変換
+    images = [{'filename': row[1]} for row in cur.fetchall()]
+    
+    cur.close()
 
+    return render_template('upload.html', images=images)
+
+# 画像をアップロードするためのルート
 @app.route('/upload', methods=['POST'])
 def upload():
-    try:
+    if 'file' in request.files:
         file = request.files['file']
-        filename = file.filename
-        data = file.read()
-        description = request.form['description']
+        if file.filename != '':
+            # アップロードされた画像を保存
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
 
-        new_image = Image(filename=filename, data=data, description=description)
-        db.session.add(new_image)
-        db.session.commit()
+            # データベースにファイル名を保存
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO images (filename) VALUES (%s)", (file.filename,))
+            mysql.connection.commit()
+            cur.close()
 
-        return redirect(url_for('index'))
-
-    except KeyError:
-        return "No 'file' key found in request", 400
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
---
+++ test.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:shimaT0915%@shori.local/mybe'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'  # Set your own secret key
+
+# Creating SQLAlchemy and Flask-Admin objects without app context
+db = SQLAlchemy(app)
+admin = Admin(app)
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255))
+    data = db.Column(db.LargeBinary)
+    description = db.Column(db.String(255))
+
+
+
+# Creating the admin view within the app context
+with app.app_context():
+    db.create_all()
+    admin.add_view(ModelView(Image, db.session))
+
+@app.route('/')
+def index():
+    # Displaying the list of images
+    images = Image.query.all()
+    return render_template('test.html', images=images)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    try:
+        file = request.files['file']
+        filename = file.filename
+        data = file.read()
+        description = request.form['description']
+
+        new_image = Image(filename=filename, data=data, description=description)
+        db.session.add(new_image)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    except KeyError:
+        return "No 'file' key found in request", 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
